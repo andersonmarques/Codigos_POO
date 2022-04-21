238fa60ac336597837cc13291136dd1d47a6e7b3
Anderson Soares <anderson.gmarques@gmail.com>
2022-04-21 16:54:03 -0300

    Exercícios imperativos
    Exercitando com a turma algumas questões com soluções imperativas em java.

---
+++ exercicios_imperativos/src/Exercicio07.java
@@ -0,0 +1,5 @@
+public class Exercicio07{
+    public static void main(String[] args) {
+        System.out.println("Esse é meu primeiroexercício");
+    }
+}
---
+++ exercicios_imperativos/src/Exercicio09.java
@@ -0,0 +1,10 @@
+public class Exercicio09 {
+    public static void main(String[] args) {
+        //int, long, float, double, boolean e String
+        String nome = "Anderson Soares";
+        String end = "Rua X";
+        String fone = "91-987654321";
+        
+        System.out.println("Nome: "+nome+"\nEnd:"+end+"\nFone:"+fone);
+    }
+}
